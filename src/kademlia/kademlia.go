e64c4d9e406ab41b6dc425ae39def0066e9aa56e
Max New <maxsnew@gmail.com>
2013-01-17 13:11:50 -0600

    Gofmt'd

--- src/kademlia/kademlia.go
+++ src/kademlia/kademlia.go
@@ -1,15 +1,14 @@
 package kademlia
+
 // Contains the core kademlia type. In addition to core state, this type serves
 // as a receiver for the RPC methods, which is required by that package.
 
 // Core Kademlia type. You can put whatever state you want in this.
 type Kademlia struct {
-    NodeID ID
+	NodeID ID
 }
 
 func NewKademlia() *Kademlia {
-    // TODO: Assign yourself a random ID and prepare other state here.
-    return new(Kademlia)
+	// TODO: Assign yourself a random ID and prepare other state here.
+	return new(Kademlia)
 }
-
-
