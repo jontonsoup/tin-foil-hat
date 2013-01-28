55d2c1d897675e90127943afedc550e8d9648f58
Gursimran Singh <g13singh@gmail.com>
2013-01-27 22:56:34 -0600

    cleaned up ExampleBucket()

--- kademlia/kademlia_test.go
+++ kademlia/kademlia_test.go
@@ -14,9 +14,13 @@ func ExampleBucket() {
 	b.Contacts.PushBack(c)
 	val := b.Contacts.Back().Value
 	cval := val.(*Contact)
+	fmt.Println(cval.NodeID)
 	fmt.Println(cval.IPAddress)
+	fmt.Println(cval.Port)
 	// Output:
+	// some freaking id
 	// 127.0.0.1
+	// 7970
 }
 
 func TestBucket(*testing.T) {
