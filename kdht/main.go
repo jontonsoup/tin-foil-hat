7c2544e992c6469bdb1c79675081a809bc44847c
Jonathan Friedman <friedmanj98@gmail.com>
2013-01-29 22:02:22 -0600

    Feature: Max sucks

--- kdht/main.go
+++ kdht/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
-	"log"
-	"math/rand"
-	"net"
-	"net/http"
-	"net/rpc"
-	"os"
-	"strings"
-	"time"
+"bufio"
+"flag"
+"fmt"
+"log"
+"math/rand"
+"net"
+"net/http"
+"net/rpc"
+"os"
+"strings"
+"time"
 )
 
 import (
-	"kademlia-go/kademlia"
+"kademlia-go/kademlia"
 )
 
 func main() {
@@ -85,6 +85,16 @@ func runCommand(k *kademlia.Kademlia, s string) (err error) {
 	switch fields[0] {
 	case "get_node_id":
 		fmt.Printf("OK: %s\n", k.NodeID.AsString())
+	case "ping":
+		client, err := rpc.DialHTTP("tcp", fields[1])
+		if err != nil {
+			log.Fatal("DialHTTP: ", err)
+		}
+		ping := new(kademlia.Ping)
+		ping.MsgID = k.NodeID
+		var pong kademlia.Pong
+		err = client.Call("Kademlia.Ping", ping, &pong)
+		log.Printf("pong msgID: %s\n", pong.MsgID.AsString())
 	default:
 		fmt.Println("Unrecognized command", fields[0])
 	}
